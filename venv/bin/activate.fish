bc0f58a3541b4477848fe134c8a1ffefc8d3ec82
Milan <milan.bakos@gmail.com>
2024-06-16 12:58:27 +0100

    Cleaned up enviroment and regenerated requirements file

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/milan/Documents/new/ml-threat-detection-app/venv"
+set -gx VIRTUAL_ENV "/home/osboxes/Documents/dissertation/ML_Final_RT/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
